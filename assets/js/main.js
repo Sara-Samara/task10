955247f967ac168779b7cb6feccff772146d282a
Sara-Samara <samarasara649@gmail.com>
2025-03-02 07:41:05 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,36 @@
 const getCategoryProduct = async () => {
-    const { data } = await axios.get("https://fakestoreapi.com/products/categories");
-    return data;
+    try {
+        const { data } = await axios.get("https://fakestoreapi.com/products/categories");
+        return data;
+    } catch (error) {
+        return [];
+    }
+
 }
 
 const displayCategory = async () => {
     const categories = await getCategoryProduct();
-    const category = categories.map((category) => {
-        return `
-           <div class ="category">
-           <h2>${category}</h2>
-           <a href="./details.html?category=${category}">products</a>
-        </div>
-        
+    if (categories.length == 0) {
+        document.querySelector("section .container").innerHTML = `
+            <h1>No categories found</h1>
         `;
-    }).join('');
-    document.querySelector("section .container .row").innerHTML = category;
+        return;
+    }
+    else {
+        const category = categories.map((category) => {
+            return `
+               <div class ="category">
+               <h2>${category}</h2>
+               <a href="./details.html?category=${category}">products</a>
+            </div>
+            
+            `;
+        }).join('');
+        document.querySelector("section .container .row").innerHTML = category;
+        document.querySelector(".loader").classList.add("No-loader");
+    }
+
+
 
 }
 
